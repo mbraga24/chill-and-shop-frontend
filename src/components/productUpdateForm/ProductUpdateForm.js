76edb2afcc499a61bd1f48a7109ad5bdba10dc5e
github username <rdg.brg@gmail.com>
2021-02-07 00:16:28 -0500

    Create pPoductUpdateForm

---
+++ src/components/productUpdateForm/ProductUpdateForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Styles.scss';
+
+const ProductUpdateForm = ({ props }) => {
+  return (
+    <div className="productUpdateForm">
+      <h1>UPDATE PRODUCT FORM</h1>
+    </div>
+  )
+}
+
+export default ProductUpdateForm;
